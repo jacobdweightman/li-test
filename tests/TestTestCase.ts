8cc3dba680bb1e4df157038d1f5fdb7a8fb02a00
jacobdweightman <jacobdweightman@gmail.com>
2020-05-09 22:38:32 -0500

    Add negative tests

--- tests/TestTestCase.ts
+++ tests/TestTestCase.ts
@@ -5,20 +5,50 @@ export default class TestTestCase extends TestCase {
         this.assertTrue(1 == 1);
     }
 
+    testAssertTrueFailure() {
+        this.assertThrows(() => {
+            this.assertTrue(false);
+        });
+    }
+
     testAssertFalse() {
         this.assertFalse(1 != 1);
+    }
+
+    testAssertFalseFailure() {
+        this.assertThrows(() => {
+            this.assertFalse(true);
+        });
     }
 
     testAssertEqual() {
         this.assertEqual(5, 5);
     }
 
+    testAssertEqualFailure() {
+        this.assertThrows(() => {
+            this.assertEqual(5, 6);
+        });
+    }
+
     testAssertNotEqual() {
         this.assertNotEqual(5, 6);
     }
 
+    testAssertNotEqualFailure() {
+        this.assertThrows(() => {
+            this.assertNotEqual(5, 5);
+        });
+    }
+
     testAssertLess() {
         this.assertLess(6, 7);
+    }
+
+    testAssertLessFailure() {
+        this.assertThrows(() => {
+            this.assertLess(7, 6);
+        });
     }
 
     testAssertLessOrEqual() {
@@ -26,8 +56,20 @@ export default class TestTestCase extends TestCase {
         this.assertLessOrEqual(7, 7);
     }
 
+    testAssertLessOrEqualFailure() {
+        this.assertThrows(() => {
+            this.assertLessOrEqual(7, 6);
+        });
+    }
+
     testAssertGreater() {
         this.assertGreater(7, 6);
+    }
+
+    testAssertGreaterFailure() {
+        this.assertThrows(() => {
+            this.assertGreater(6, 7);
+        });
     }
 
     testAssertGreaterOrEqual() {
@@ -35,12 +77,30 @@ export default class TestTestCase extends TestCase {
         this.assertGreaterOrEqual(7, 7);
     }
 
+    testAssertGreaterOrEqualFailure() {
+        this.assertThrows(() => {
+            this.assertGreaterOrEqual(4, 5);
+        });
+    }
+
     testAssertIsNull() {
         this.assertIsNull(null);
     }
 
+    testAssertIsNullFailure() {
+        this.assertThrows(() => {
+            this.assertIsNull(0);
+        });
+    }
+
     testAssertNotNull() {
         this.assertNotNull(0);
+    }
+
+    testAssertNotNullFailure() {
+        this.assertThrows(() => {
+            this.assertNotNull(null);
+        });
     }
 
     testAssertThrows() {
